d39ff6d9b0a599fe3454e46111e73b6e32546ff4
victormongare1 <vicmongz254@gmail.com>
2020-02-28 17:16:42 +0300

    created function to alert order has been received

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 //business-logic
-function pizza {
+function pizza (size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = [];
 }
 
 //user-interface
+$(document).ready(function() {
+  $("form#top").submit(function(event) {
+    event.preventDefault();
+  alert("Your order has been received")
+  })
+})
