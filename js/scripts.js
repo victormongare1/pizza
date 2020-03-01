e7c1b8ab085c41fdff5451a1dcac60c8cb7bbb2d
victormongare1 <vicmongz254@gmail.com>
2020-03-01 16:15:37 +0300

    created function to append order

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,20 @@ function pizza (size, crust, toppings) {
   this.crust = crust;
   this.toppings = [];
 }
-
+pizza.prototype.fullOrder= function() {
+  return this.size + " " +this.crust ;
+}
 //user-interface
 $(document).ready(function() {
-  $("button#make-order").click(function(event) {
+  $("button#another").click(function(event) {
     event.preventDefault();
-  var address=prompt("Enter your Address :");
-  alert("Your order has been received and will be delivered to you in " + address)
-  });
-  $("button#another").click(function() {
-    (".size").append('<select class="form-control" id="size">'+
-                      '<option>Large</option>'+
-                      '<option>Medium</option>'+
-                      '<option>Small</option>'+
-                      '</select>'
-    );
+    ("#pizza-size").append('<select class="form-control" id="size">'+
+                            '<option>Large</option>'+
+                            '<option>Medium</option>'+
+                            '<option>Small</option>'+
+                            '</select>'
+                          );
+        
     (".crust").append( '<select class="form-control" id="crust">'+
                   '<option>Thin</option>'+
                   '<option>Deep</option>'+
@@ -40,5 +39,17 @@ $(document).ready(function() {
                     '<label for="topping6">Mushroom</label><br />'+
                     '</form>'
                     );
+  });
+  $("button#make-order").click(function(event) {
+    event.preventDefault();
+  var size=$("#size").val();
+  var crust=$("#crust").val();
+  var newOrder= new pizza (size , crust)
+  var top=$("input:checkbox[name=top]:checked").val();
+   newOrder.toppings.push(top)
+  var address=prompt("Enter your Address :");
+  alert("Your order has been received and will be delivered to you in " + address)
+  
+  $("ul#customer-order").append("<li><span class=order>" + newOrder.fullOrder()  + "</span></li>");
   });
 })
